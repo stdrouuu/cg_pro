ff52a6053af01949ef035d67f36f617bb45ce217
stdrouuu <brajs0111@gmail.com>
2025-09-23 13:13:27 +0700

    draw triangle using bresenham

---
+++ Home/src/draw_triangle_bre.cpp
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <conio.h>
+#include <math.h> // pamggil M_PI
+#include <graphics.h>
+
+int x_mid, y_mid;
+
+//diambil dari soal_3.cpp
+void bre_line(int xa, int ya, int xb, int yb, int color) // titik awal (xa, ya), titik akhir (xb, yb)
+{
+    int delta_x, delta_y, p;
+    int x, y, i;
+    int xtemp, ytemp;
+
+    delta_x = xb - xa;
+    delta_y = yb - ya;
+
+    if(delta_x >= 0) xtemp = 1;
+    else xtemp: -1;
+
+    if (delta_y >= 0) ytemp = 1;
+    else ytemp = -1;
+
+    x = xa;
+    y = ya;
+    putpixel(x + x_mid, y_mid - y, color);
+    i = 0;
+
+    if (abs(delta_y) < abs(delta_x))
+    {
+        p = (2 * abs(delta_y)) - abs(delta_x);
+        do
+        {
+            if (p < 0)
+            {
+                x = x + xtemp;
+                p = p + 2 * abs(delta_y);
+            }
+            else
+            {
+                x = x + xtemp;
+                y = y + ytemp;
+                p = p + 2 * abs(delta_y) - 2 * abs(delta_x);
+            }
+            putpixel(x + x_mid, y_mid - y, color);
+            i++;
+        } while (i <= abs(delta_x));
+    }
+    else
+    {
+        p = (2 * abs(delta_x)) - abs(delta_y);
+        do
+        {
+            if (p < 0)
+            {
+                y = y + ytemp;
+                p = p + 2 * abs(delta_x);
+            }
+            else
+            {
+                x = x + xtemp;
+                y = y + ytemp;
+                p = p + 2 * abs(delta_x) - 2 * abs(delta_y);
+            }
+            putpixel(x + x_mid, y_mid - y, color);
+            i++;
+        } while (i <= abs(delta_y));
+    }
+}
+
+float degressToRadians(float angle)
+{
+    return angle * M_PI / 180;
+}
+
+void triangle(int xa, int ya, int angle, int sideLen) 
+{
+    int xb = sideLen * cos(degressToRadians(angle - 30)) + xa;
+    int yb = sideLen * sin(degressToRadians(angle - 30)) + ya;
+
+    int xc = sideLen * cos(degressToRadians(angle + 30)) + xa;
+    int yc = sideLen * sin(degressToRadians(angle + 30)) + ya;
+
+    printf("xa: %d, ya: %d\n", xa, ya);
+    printf("xb: %d, yb: %d\n", xb, yb);
+    printf("xc: %d, yc: %d\n", xc, yc);
+    
+    // 2:green;  11:cyan;  14:kuning
+    bre_line(xa, ya, xb, yb, 2);
+    bre_line(xb, yb, xc, yc, 11);
+    bre_line(xa, ya, xc, yc, 14);
+}
+
+int main ()
+{
+    int gm, gd = DETECT;
+    char data[] = "C:\\MinGW\\lib\\libbgi.a";
+    initgraph(&gd, &gm, data);
+    cleardevice();
+
+    int xmax = getmaxx(), ymax = getmaxy();
+    x_mid = xmax / 2;
+    y_mid = ymax / 2;
+
+    setcolor(RED);
+    line(0, y_mid, xmax, y_mid);  
+    line(x_mid, 0, x_mid, ymax);   
+    triangle(10,10,-30,30);
+
+
+    getch();
+    closegraph();
+}
